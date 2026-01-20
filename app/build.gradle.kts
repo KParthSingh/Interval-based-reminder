9b18c1218e32d223ebc1ac01f8f717dd3052a7c2
k.Parth <singhom7789@gmail.com>
2026-01-20 18:44:23 +0530

    Changes for proper build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.medicinereminder.app"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 31
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,5 +14,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "MedicineReminder"
+rootProject.name = "RelativeTimer"
 include(":app")
